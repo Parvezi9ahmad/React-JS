78655dcd36f3536be62909d5427ae4a6cf0b69ae
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 10:57:13 +0530

    16 commit

---
+++ src/components/user-names/UserNames.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+function UserNames(){
+    let[names,setNames]=useState(["Rohan","Aish","Vaish","Aadi","Wilson","Mat"]);
+
+    return (
+        <div style={{padding:"50px"}}>
+            <h2>User Names</h2>
+            <ul>
+                {
+                    names.map(function(name){
+                        return <li>{name}</li>
+                    })
+                }
+            </ul>
+
+        </div>
+    );
+}
+
+export default UserNames
