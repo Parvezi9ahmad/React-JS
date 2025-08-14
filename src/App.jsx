d7f7d4a0cd8458bf2808371981357d29d9f38a5f
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 12:12:07 +0530

    21 commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Nav from './components/nav/Nav'
+import Ref from './components/ref/Ref'
+import Timer from './components/timer/Timer'
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+      <Nav />
+      <Timer />
+      <Ref />
+
+    </div>
+  )
+}
+
+export default App
