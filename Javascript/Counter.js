67437fe9ce214ebd661109230744cb6e1bede081
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 13:19:00 +0530

    text

--- Javascript/Counter.js
+++
@@ -1,6 +0,0 @@
-var count=0;
-function increaseCount(){
-    var pRef=document.querySelector("P")
-    pRef.innerText="Count Value is :"+ ++count;
-    
-}
--- src/components/context-api/MyHomeContext.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export let myHomeContext=createContext();
-
