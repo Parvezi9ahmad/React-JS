fd2d8af4d114b3bc70de9ab7def78d7b58fe399e
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 13:31:58 +0530

    27 commit

---
+++ src/SpreadOperator.js
@@ -0,0 +1,14 @@
+
+
+var obj1 = {
+    name: "Parvez",
+    city: "Pune",
+};
+
+var obj2 = {
+    ...obj1,
+    name:"Ahmad",
+    address:"xyz street",
+};
+
+console.log(obj2);
---
+++ src/redux/reducers/myReducer.js
@@ -0,0 +1,22 @@
+const initialData = {
+    name: "",
+    city: "",
+};
+
+const myReducer = (state = initialData, action) => {
+    if (action.type === "Name") {
+        state = {
+            ...state,
+            name: action.nameComp,
+        };
+    }
+    else if (action.type === "CITY") {
+        state = {
+            ...state,
+            city: action.cityName
+        }
+    }
+    return state;
+}
+
+export default myReducer;
