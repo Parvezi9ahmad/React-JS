6996cc95befe8fa14d4b37c46a43fd4efefc7902
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 11:07:11 +0530

    20 commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Nav from './components/nav/Nav'
+import Ref from './components/ref/Ref'
+
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+      <Nav />
+      <Ref />
+
+    </div>
+  )
+}
+
+export default App
