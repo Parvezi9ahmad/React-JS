b55bed9d3d3dafe8807aee14d58408995d3f0e4d
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 11:02:46 +0530

    18 commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+import './App.css'
+import Header from './components/header/Header'
+import Counter from './components/counter/Counter'
+import ImageComp from './components/imagetask/ImageComp'
+import Products from './components/products/Products'
+import Home from './components/home/Home'
+import Nav from './components/nav/Nav'
+import { Route, Routes } from 'react-router-dom'
+import Electronics from './components/products/categories/electronics/Electronics'
+import Jewelery from './components/products/categories/jewelery/Jewelery'
+import MenC from './components/products/categories/mensC/MenC'
+import WomenC from './components/products/categories/womensC/WomenC'
+import PageNotFound from './components/pagenotfound/PageNotFound'
+import AllProducts from './components/allproducts/AllProducts'
+import UserNames from './components/user-names/UserNames'
+// import A from './components/Parent-Child/A'
+import A from './components/context-api/A'
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h2>App Component</h2>
+      <Header/> */}
+      <Nav />
+      <A />
+      {/* <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/products"} element={<Products />} >
+          <Route path={"electronics"} element={<Electronics />} />
+          <Route path={"jewelery"} element={<Jewelery />} />
+          <Route path={"menc"} element={<MenC />} />
+          <Route path={"womenc"} element={<WomenC />} />
+        </Route>
+        <Route path={"/counter"} element={<Counter />} />
+        <Route path={"/imagecomp"} element={<ImageComp />} />
+        <Route path={"/allproducts"} element={<AllProducts />} />
+        <Route path={"/usernames"} element={<UserNames />} />
+        <Route path={"*"} element={<PageNotFound />}/>
+
+      </Routes> */}
+      {/* <Home />
+      <Products />
+
+      <ImageComp />
+      <Counter /> */}
+    </div>
+  )
+}
+
+export default App
