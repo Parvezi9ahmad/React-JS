7944d9e2ab25c87677c44e00156bbf2fa61e9325
parvez Ahmad <parvezi9ahmad@gmail.com>
2025-08-14 13:22:29 +0530

    24 commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+import Nav from './components/nav/Nav'
+import Ref from './components/ref/Ref'
+import Timer from './components/timer/Timer'
+import RefDom from './components/ref/RefDom'
+import UseReducer from './components/reducer/UseReducer'
+import Image from './components/useeffect/lifecycle/Image'
+import Text from './components/useeffect/lifecycle/Text'
+import { useState } from 'react'
+import Users from './components/useeffect/lifecycle/Users'
+import Counter from './components/reducer/Counter'
+
+
+
+
+function App() {
+  const [showImage, setShowImage] = useState(true);
+  return (
+    <div className="app">
+
+
+      <Nav />
+
+      {/* <Users />
+      {showImage ? <Image /> : <Text />}
+      <br /><br />
+      <button onClick={() => { setShowImage(true) }}>Show Image</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={() => { setShowImage(false) }}>Show Text</button> */}
+
+      {/* <RefDom /> */}
+      {/* <UseReducer /> */}
+      <Counter />
+      {/* <Timer />
+      <Ref /> */}
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/child-parent/Name.jsx
@@ -0,0 +1,17 @@
+import Display from "./Display";
+import { useState } from "react";
+
+function Name() {
+    let [state, setState] = useState("");
+    return (
+        <div style={{ padding: "50px" }}>
+            <h2>Name component</h2><br />
+            <p>Child Data :{state}</p>
+            <br /><br />
+            <hr /><br /><br />
+            <Display setData={setState} />
+        </div>
+    );
+}
+
+export default Name
